ec3a0a97a7d8ad8998d146cdc4cc14120c77ef0b
elijah obara <elijahobara357@gmail.com>
2023-05-20 14:53:24 +0300

    user schema

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -73,4 +73,17 @@ const userSchema = mongoose.Schema({
     ]
   });
 
-  
+
+//Hashing password
+userSchema.pre('save', async function (next) {
+
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 12);
+        this.cpassword = await bcrypt.hash(this.cpassword, 12);
+    }
+
+    next();
+
+});
+
+//Generating Tokens
