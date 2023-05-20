1ee57a00786d4697eae59c2359d227e21e344d55
elijah obara <elijahobara357@gmail.com>
2023-05-20 23:37:51 +0300

    register route

--- server/Routes/authRouter.js
+++ server/Routes/authRouter.js
@@ -10,5 +10,41 @@ authRouter.get('/', (req, res) => {
     res.send('Router js is called');
 });
 
+//Register
+authRouter.post('/register', async (req, res) => {
+    const { name, email, phone, password, cpassword } = req.body;
+
+    if (!name || !email || !phone || !password || !cpassword) {
+        return res.status(400).json({ error: 'Fill all required fields properly' });
+    }
+
+    if (password.length < 8) {
+        return res.status(400).json({ error: 'Password should be a minimum of 8 characters' });
+    }
+
+    try {
+        const userExist = await User.findOne({ email: email });
+
+        if (userExist) {
+            return res.status(400).json({ error: 'Email already exists' });
+        } else if (password !== cpassword) {
+            return res.status(400).json({ error: 'Passwords do not match' });
+        } else {
+            const user = new User({ name, email, phone, password, cpassword });
+
+            // Hashing is Used
+
+            await user.save();
+
+            res.status(201).json({ message: 'User registered successfully' });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+//signin
+
 
 export default authRouter;
