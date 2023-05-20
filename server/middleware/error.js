ff64062da7ac1b90a4e23fcc41ccd7b7f4db2d55
elijah obara <elijahobara357@gmail.com>
2023-05-20 15:45:21 +0300

    Error handler

---
+++ server/middleware/error.js
@@ -0,0 +1,17 @@
+import ErrorHandler from "../utils/errorHandler";
+
+export default (err, req, res, next) => {
+  err._statusCode = err.statusCode || 500;
+  err._message = err.message || "Internal Server Error";
+
+  // Handling MongoDB Error -> Cast Error
+  if (err.name === "CastError") {
+    const message = `Resource Not Found. Invalid: ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  res.status(err._statusCode).json({
+    success: false,
+    message: err._message,
+  });
+};
