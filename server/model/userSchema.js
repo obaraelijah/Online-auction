e623fa3ea12f1d1eda11ab1023f937abc6c15c2a
elijah obara <elijahobara357@gmail.com>
2023-05-20 14:59:08 +0300

    jwt token generation

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -87,3 +87,13 @@ userSchema.pre('save', async function (next) {
 });
 
 //Generating Tokens
+userSchema.methods.generateAuthToken = async function () {
+    try{
+            let token = jwt.sign({_id: this._id } , process.env.SECRET_KEY);
+            this.tokens = this.tokens.concat({token : token});
+            await this.save();
+            return token;
+    }catch(err){
+                console.log(err);
+    }
+}
