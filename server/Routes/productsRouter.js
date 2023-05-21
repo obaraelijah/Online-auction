f1d6aba5cb6485a54ed85c604b4a26510e084667
elijah obara <elijahobara357@gmail.com>
2023-05-21 09:11:08 +0300

    product router

--- server/Routes/productsRouter.js
+++ server/Routes/productsRouter.js
@@ -1,10 +1,26 @@
 import express from "express";
-import { createProduct } from "../controllers/productController.js";
+import { 
+    createProduct,
+    getAllProducts, 
+    updateProduct,
+    deleteProduct,
+    getProductDetails,
+    getMyProducts 
+} from "../controllers/productController.js";
 
 const productsRouter = express.Router()
 
 //create product
 productsRouter.route("/product/new").post(createProduct);
+
+// get all products 
+productsRouter.route("/products").get(getAllProducts);
+
+//get single product
+productsRouter.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails);
+
+//get my products
+productsRouter.route("/products/myproducts").get(getMyProducts);
 
 
 export default productsRouter;
