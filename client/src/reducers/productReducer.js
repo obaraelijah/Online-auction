f771c3f57129b28ac74f3e5fba1dfd14bff5dc23
elijah obara <elijahobara357@gmail.com>
2023-05-21 21:56:11 +0300

    Redux config

--- client/src/reducers/productReducer.js
+++ client/src/reducers/productReducer.js
@@ -97,3 +97,51 @@ export const productDetailsReducer = (state = { product: {} }, action) =>{
     
     };
 
+
+//new producct request
+//  "" success
+// create product
+// new product fail
+// new product reset
+
+    export const newProductReducer = (state = { product: {} }, action) =>{
+
+        switch (action.type) {
+            case NEW_PRODUCT_REQUEST:
+                return{
+                    ...state,
+                    loading:true,
+                };
+        
+                case NEW_PRODUCT_SUCCESS:
+                return{
+                    loading:false,
+                    success:action.payload.success,
+                    product:action.payload.product,
+                };
+
+                case NEW_PRODUCT_FAIL:
+                return{
+                    ...state,
+                    loading:false,
+                    error: action.payload,
+                };
+
+                case NEW_PRODUCT_RESET:
+                return{
+                 ...state,
+                 success:false,
+                };
+        
+                
+                case CLEAR_ERRORS:
+                return{
+                    ...state,
+                    error: null,          
+                };
+        
+            default:
+                return state;
+        }
+        
+        };
