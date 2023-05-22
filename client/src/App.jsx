a0463089be54a2c09f54bcf4cb7562237f3d5cdc
elijah obara <elijahobara357@gmail.com>
2023-05-22 15:33:13 +0300

    contact us page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useReducer } from 'react';
 import Home from "./client/HomePage/Home";
 import Navbar from "./client/Navbar/Navbar";
+import Contact from "./client/ContactPage/Contact";
 import {initialState , reducer } from "./client/Reducer/UseReducer";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
@@ -20,6 +21,9 @@ const Routing = () => {
     return (
     <Switch>
         <Route exact path="/" component={Home} />
+
+        <Route exact path="/contact" component={Contact} />
+        
         <Redirect to="/" />
       {/* Can use Error 404 Page  but redirect is better than that page */}
     </Switch>
