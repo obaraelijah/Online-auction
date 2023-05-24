fec9a6f14f1f9aa9ae23559b6cb6f8208611dac6
elijah obara <elijahobara357@gmail.com>
2023-05-24 15:22:30 +0300

    Debugging

--- server/utils/apifeatures.js
+++ server/utils/apifeatures.js
@@ -7,7 +7,6 @@ class ApiFeatures{
 
   // SEARCH FEATURE OF API -> query parameter inside api feature = queryStr
   // Deals with keyword inside Querystr
-
   search(){
       const keyword = this.queryStr.keyword 
       ? {
@@ -15,8 +14,7 @@ class ApiFeatures{
 
               // here to find name can also use inside find() -> But find name with pattern -> using mongodb operator regex
               // Ex: watch and watchex
-              // "i" is for case insensetive
-              
+              // "i" is for case insensetive             
               $regex: this.queryStr.keyword,
               $options: "i" ,
           },
@@ -29,16 +27,12 @@ class ApiFeatures{
       return this;
   }
 
-
   // Filter Functionality
 filter() {
   
   // object contains refrence -> Can not directly assign -> Used Spread Operator for copy of object 
   const queryCopy = {...this.queryStr};
-
-
   // console.log(queryCopy); -> { keyword: 'product9', category: 'laptop' }
-
   // removing some feilds of category -> SEARCH(KEYWORD) , PAGINATION(PAGE) , LIMIT 
   // Category -> Case Insensitive
 const removeFields = ["keyword" , "page" , "limit"];
@@ -46,8 +40,6 @@ const removeFields = ["keyword" , "page" , "limit"];
 removeFields.forEach((key) => delete queryCopy[key]);
 
 // console.log(queryCopy); ->  { category: 'laptop' }
-
-
 // FILTER FOR PRICE -> startingBid
 // console.log(queryCopy); ->> { startingBid: { gt: '1200', lt: '2000' } } -> mongo db conside operator as $opr
 // So -> convert into string (json.stringify()) -> edit -> convert into object (json.parse())
@@ -65,17 +57,12 @@ this.query = this.query.find(JSON.parse(queryStr));
 return this;
 
 }
-
 pagination(resultPerPage){
-
   const currentPage = Number(this.queryStr.page) || 1;  
   // Database 50 product -> Per Page 10 Product -> total page 5  -> At fist page skipped 0 -> 2nd Page Skipped 10 ..
-
   const skip = resultPerPage * (currentPage - 1);
-  
   // Assign to  Query
   this.query = this.query.limit(resultPerPage).skip(skip); 
-
   return this;
 }
 
